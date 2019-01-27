35ebf42e16a5324f67fe81aedeafafbdb5a3c697
Yunianamn <47076957+Yunianamn@users.noreply.github.com>
2019-01-27 17:29:57 +0700

    Add files via upload
    tugas  uas java

---
+++ latihanjava/src/latihan2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author asus
+ */
+public class latihan2 {
+    private static void PerulanganAngka(){
+        for (int a=0 ;a<10;a++){
+            System.out.println("nilai "+(a+1));
+        }
+    }
+    public static void main(String[] args){
+        PerulanganAngka();
+    }
+}
